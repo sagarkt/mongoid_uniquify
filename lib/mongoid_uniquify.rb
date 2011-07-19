b52c11eac8d0046ddec5730da2799811a40273cd
Kichiro IKEMOTO <kichiro@yomukaku.net>
2011-07-19 14:44:38 +0900

    Update README. Refactored file structure, and bumped to 0.1.1.

--- lib/mongoid_uniquify.rb
+++ lib/mongoid_uniquify.rb
@@ -1,1 +1,31 @@
-require File.join(File.dirname(__FILE__), 'mongoid/uniquify')
+module Mongoid #:nodoc:
+  module Uniquify
+    def self.included(base)
+      base.extend ClassMethods
+    end
+    
+    def ensure_unique(name)
+      begin
+        self[name] = yield
+      end while self.class.first(:conditions => {name => self[name]})
+    end
+
+    module ClassMethods
+      def uniquify(*args, &block)
+        options = { :length => 8, :chars => ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a }
+        options.merge!(args.pop) if args.last.kind_of? Hash
+        args.each do |name|
+          after_initialize do |record|
+            if block
+              record.ensure_unique(name, &block)
+            else
+              record.ensure_unique(name) do
+                Array.new(options[:length]) { options[:chars].to_a[rand(options[:chars].to_a.size)] }.join
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
